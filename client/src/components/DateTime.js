dc616d83e7fccb43f00f318f6372b95997ab01c0
Karolina Benitez <kbeniitez@gmail.com>
2019-05-02 18:42:20 -0700

    Add date picker

--- client/src/components/DateTime.js
+++ client/src/components/DateTime.js
@@ -1,30 +1,63 @@
-import React from 'react'
-import { Dropdown, Form, Col, Row } from 'react-bootstrap'
+// import React from 'react'
+// // import { Dropdown, Form, Col, Row } from 'react-bootstrap'
+// import DatePicker from 'react-datepicker'
+
+
+// class DateTime extends React.Component {
+//   render() {
+//     return (
+//       <div className='dateTime'>
+//         <DatePicker
+//           selected={this.state.startDate}
+//           onChange={this.handleChange}
+//           showTimeSelect
+//           timeFormat="HH:mm"
+//           timeIntervals={15}
+//           dateFormat="MMMM d, yyyy h:mm aa"
+//           timeCaption="time"
+//         />
+//       </div>
+//     )
+//   }
+// }
+
+// export default DateTime
+
+import React from "react";
+import DatePicker from "react-datepicker";
+
+import "react-datepicker/dist/react-datepicker.css";
+
+// CSS Modules, react-datepicker-cssmodules.css
+// import 'react-datepicker/dist/react-datepicker-cssmodules.css';
 
 class DateTime extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      startDate: new Date()
+    };
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange(date) {
+    this.setState({
+      startDate: date
+    });
+  }
+
   render() {
     return (
-      <div className='dateTime'>
-        <Form>
-          <Form.Group as={Row}>
-            <Col sm={10}>
-              <Dropdown>
-                <Dropdown.Toggle variant="secondary" id="dropdown-basic" >
-                  Arrive By
-                  </Dropdown.Toggle>
-                <Dropdown.Menu>
-                  <Dropdown.Item href="#/action-1">Address #1</Dropdown.Item>
-                  <Dropdown.Item href="#/action-2">Address #2</Dropdown.Item>
-                  <Dropdown.Item href="#/action-3">Address #3</Dropdown.Item>
-                </Dropdown.Menu>
-              </Dropdown>
-            </Col>
-          </Form.Group>
-        </Form>
-      </div>
-    )
+      <DatePicker
+        selected={this.state.startDate}
+        onChange={this.handleChange}
+        showTimeSelect
+        timeFormat="HH:mm"
+        timeIntervals={15}
+        dateFormat="MMMM d, yyyy h:mm aa"
+        timeCaption="time"
+      />
+    );
   }
 }
-
 export default DateTime
-
