311786afdda30df6de1d1bb66dd691ed7bd49279
Karolina Benitez <kbeniitez@gmail.com>
2019-05-14 10:53:29 -0700

    Debug GoogleRouteTable component

--- client/src/components/GoogleMaps/GoogleTripsTable.jsx
+++ client/src/components/GoogleMaps/GoogleTripsTable.jsx
@@ -3,6 +3,92 @@ import React, { Component } from 'react';
 // const API = 'AIzaSyAhVWhFdizN56yFD2Wf7Q1O_cOZHofdab4';
 const google = window.google;
 
+
+AutocompleteDirectionsHandler = (map) => {
+  this.map = map;
+  this.originPlaceId = null;
+  this.destinationPlaceId = null;
+  this.travelMode = 'WALKING';
+  this.directionsService = new google.maps.DirectionsService;
+  this.directionsDisplay = new google.maps.DirectionsRenderer;
+  this.directionsDisplay.setMap(map);
+
+  var originInput = document.getElementById('origin-input');
+  var destinationInput = document.getElementById('destination-input');
+  var modeSelector = document.getElementById('mode-selector');
+
+  var originAutocomplete = new google.maps.places.Autocomplete(originInput);
+  // Specify just the place data fields that you need.
+  originAutocomplete.setFields(['place_id']);
+
+  var destinationAutocomplete =
+    new google.maps.places.Autocomplete(destinationInput);
+  // Specify just the place data fields that you need.
+  destinationAutocomplete.setFields(['place_id']);
+
+  this.setupClickListener('changemode-walking', 'WALKING');
+  this.setupClickListener('changemode-transit', 'TRANSIT');
+  this.setupClickListener('changemode-driving', 'DRIVING');
+
+  this.setupPlaceChangedListener(originAutocomplete, 'ORIG');
+  this.setupPlaceChangedListener(destinationAutocomplete, 'DEST');
+
+  this.map.controls[google.maps.ControlPosition.TOP_LEFT].push(originInput);
+  this.map.controls[google.maps.ControlPosition.TOP_LEFT].push(
+    destinationInput);
+  this.map.controls[google.maps.ControlPosition.TOP_LEFT].push(modeSelector);
+}
+
+AutocompleteDirectionsHandler.prototype.setupClickListener = (id, mode) => {
+  var radioButton = document.getElementById(id);
+  var me = this;
+
+  radioButton.addEventListener('click', function () {
+    me.travelMode = mode;
+    me.route();
+  });
+};
+
+AutocompleteDirectionsHandler.prototype.setupPlaceChangedListener = (autocomplete, mode) => {
+  var me = this;
+  autocomplete.bindTo('bounds', this.map);
+
+  autocomplete.addListener('place_changed', function () {
+    var place = autocomplete.getPlace();
+
+    if (!place.place_id) {
+      window.alert('Please select an option from the dropdown list.');
+      return;
+    }
+    if (mode === 'ORIG') {
+      me.originPlaceId = place.place_id;
+    } else {
+      me.destinationPlaceId = place.place_id;
+    }
+    me.route();
+  });
+};
+AutocompleteDirectionsHandler.prototype.route = () => {
+  if (!this.originPlaceId || !this.destinationPlaceId) {
+    return;
+  }
+  var me = this;
+
+  this.directionsService.route(
+    {
+      origin: { 'placeId': this.originPlaceId },
+      destination: { 'placeId': this.destinationPlaceId },
+      travelMode: this.travelMode
+    },
+    function (response, status) {
+      if (status === 'OK') {
+        me.directionsDisplay.setDirections(response);
+      } else {
+        window.alert('Directions request failed due to ' + status);
+      }
+    });
+};
+
 export class GoogleTripsTable extends Component {
   constructor(props) {
     super(props)
@@ -34,93 +120,6 @@ export class GoogleTripsTable extends Component {
     return this.googleMapsPromise;
   }
 
-  AutocompleteDirectionsHandler(map) {
-    this.map = map;
-    this.originPlaceId = null;
-    this.destinationPlaceId = null;
-    this.travelMode = 'WALKING';
-    this.directionsService = new google.maps.DirectionsService;
-    this.directionsDisplay = new google.maps.DirectionsRenderer;
-    this.directionsDisplay.setMap(map);
-
-    var originInput = document.getElementById('origin-input');
-    var destinationInput = document.getElementById('destination-input');
-    var modeSelector = document.getElementById('mode-selector');
-
-    var originAutocomplete = new google.maps.places.Autocomplete(originInput);
-    // Specify just the place data fields that you need.
-    originAutocomplete.setFields(['place_id']);
-
-    var destinationAutocomplete =
-      new google.maps.places.Autocomplete(destinationInput);
-    // Specify just the place data fields that you need.
-    destinationAutocomplete.setFields(['place_id']);
-
-    this.setupClickListener('changemode-walking', 'WALKING');
-    this.setupClickListener('changemode-transit', 'TRANSIT');
-    this.setupClickListener('changemode-driving', 'DRIVING');
-
-    this.setupPlaceChangedListener(originAutocomplete, 'ORIG');
-    this.setupPlaceChangedListener(destinationAutocomplete, 'DEST');
-
-    this.map.controls[google.maps.ControlPosition.TOP_LEFT].push(originInput);
-    this.map.controls[google.maps.ControlPosition.TOP_LEFT].push(
-      destinationInput);
-    this.map.controls[google.maps.ControlPosition.TOP_LEFT].push(modeSelector);
-  }
-
-  AutocompleteDirectionsHandler.prototype.setupClickListener(id, mode) {
-    var radioButton = document.getElementById(id);
-    var me = this;
-
-    radioButton.addEventListener('click', function () {
-      me.travelMode = mode;
-      me.route();
-    });
-  };
-
-  AutocompleteDirectionsHandler.prototype.setupPlaceChangedListener(autocomplete, mode) {
-    var me = this;
-    autocomplete.bindTo('bounds', this.map);
-
-    autocomplete.addListener('place_changed', function () {
-      var place = autocomplete.getPlace();
-
-      if (!place.place_id) {
-        window.alert('Please select an option from the dropdown list.');
-        return;
-      }
-      if (mode === 'ORIG') {
-        me.originPlaceId = place.place_id;
-      } else {
-        me.destinationPlaceId = place.place_id;
-      }
-      me.route();
-    });
-  };
-  AutocompleteDirectionsHandler.prototype.route = function () {
-    if (!this.originPlaceId || !this.destinationPlaceId) {
-      return;
-    }
-    var me = this;
-
-    this.directionsService.route(
-      {
-        origin: { 'placeId': this.originPlaceId },
-        destination: { 'placeId': this.destinationPlaceId },
-        travelMode: this.travelMode
-      },
-      function (response, status) {
-        if (status === 'OK') {
-          me.directionsDisplay.setDirections(response);
-        } else {
-          window.alert('Directions request failed due to ' + status);
-        }
-      });
-  };
-
-
-
   componentWillMount() {
     // Start Google Maps API loading since we know we'll soon need it
     this.getGoogleMaps();
@@ -138,7 +137,7 @@ export class GoogleTripsTable extends Component {
       });
 
     });
-    new this.AutocompleteDirectionsHandler(map)
+    this.directionsHandler = new AutocompleteDirectionsHandler(map)
   }
 
   render() {
